70f0086eb789fa8b90ca43ae6659f56b95e1577e
Spatzerny <spatzerny@gmail.com>
2016-05-10 01:57:24 +0200

    add temp answer display

--- scripts/main.js
+++ scripts/main.js
@@ -13,8 +13,8 @@ function randomColor() {
 			+ leftpad(r.toString(16), 2, '0')
 			+ leftpad(g.toString(16), 2, '0')
 			+ leftpad(b.toString(16), 2, '0'),
-		rgb: 'rgb(' + r + ',' + g + ',' + b + ')',
-		hsl: rgbToHsl(r, g, b).join(',')
+		rgb: 'rgb(' + r + ', ' + g + ', ' + b + ')',
+		hsl: 'hsl(' + rgbToHsl(r, g, b).join(', ') + ')'
 	}
 }
 
@@ -69,39 +69,15 @@ function rgbToHsl(r, g, b){
 		return [Math.round(h*360), Math.round(s*100), Math.round(l*100)];
 }
 
-//https://github.com/mjackson/mjijackson.github.com/blob/master/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript.txt
-function hslToRgb(h, s, l){
-		var r, g, b;
-
-		if(s == 0){
-				r = g = b = l; // achromatic
-		}else{
-				function hue2rgb(p, q, t){
-						if(t < 0) t += 1;
-						if(t > 1) t -= 1;
-						if(t < 1/6) return p + (q - p) * 6 * t;
-						if(t < 1/2) return q;
-						if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-						return p;
-				}
-
-				var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-				var p = 2 * l - q;
-				r = hue2rgb(p, q, h + 1/3);
-				g = hue2rgb(p, q, h);
-				b = hue2rgb(p, q, h - 1/3);
-		}
-
-		return [r * 255, g * 255, b * 255];
-}
-
 
 $('document').ready(function() {
 
-	var cFormats = ['hex', 'rgb', 'hsl'];
-	var cIndex = 0;
-	var cPrimary = randomColor();
+	var formats = ['hex', 'rgb', 'hsl'];
 	var query = true;
+	var cIndex = 0;
+	var cFormat = formats[cIndex];
+	var cTrue = randomColor();
+	var cPick;
 
 	function cContrast(r, g, b) {
 		var brightness;
@@ -116,10 +92,7 @@ $('document').ready(function() {
 		}
 	}
 
-	function submit() {
-		var f = cFormats[cIndex];
-		var c1 = validColor( cPrimary[f], f);
-		var c2 = validColor( $('#input').val(), f);
+	function submit(c1, c2, f) {
 		
 		$('#submit').animate({
 			top: '60px',
@@ -128,28 +101,30 @@ $('document').ready(function() {
 			if (query) {
 				
 				for (var i=1; i < c1.length; i++) {
-				if (f = 'hex') {
-					c2[i] = c2[i].toLowerCase();
+					if (f = 'hex') {
+						c2[i] = c2[i].toLowerCase();
 
-					if (c1[i].length == 1) {
-						c1[i] = leftpad(c1[i], 2, c1[i]);
+						if (c1[i].length == 1) {
+							c1[i] = leftpad(c1[i], 2, c1[i]);
+						}
+						if (c2[i].length == 1) {
+							c2[i] = leftpad(c2[i], 2, c2[i]);
+						}
 					}
-					if (c2[i].length == 1) {
-						c2[i] = leftpad(c2[i], 2, c2[i]);
-					}
-				}
 					console.log(c1[i], c2[i], c1[i] == c2[i], 'beep');
 				}
 				
 				query = false;
 				$('#submit').text('next');
+				$('#temp').text(cTrue[f].toString()).fadeIn('fast');
 			} else {
-				cPrimary = randomColor();
-				$('body').css('background-color', cPrimary.hex);
-				cContrast(cPrimary.num[0], cPrimary.num[1], cPrimary.num[2]);
+				cTrue = randomColor();
+				$('body').css('background-color', cTrue.hex);
+				cContrast(cTrue.num[0], cTrue.num[1], cTrue.num[2]);
 				$('#input').val('');
 				query = true;
 				$('#submit').text('done');
+				$('#temp').fadeOut();
 			}
 			$('#submit').css('top', '-60px');
 			$('#submit').animate( {
@@ -166,12 +141,13 @@ $('document').ready(function() {
 			top: '60px',
 			opacity: 0
 		}, 250, function() {
-			if (cIndex == cFormats.length-1) {
+			if (cIndex == formats.length-1) {
 				cIndex = 0;
 			} else {
 				cIndex++;
 			}
-			$('#format').text(cFormats[cIndex]);
+			cFormat = formats[cIndex];
+			$('#format').text(formats[cIndex]);
 			$('#format').css('top', '-60px');
 			$('#format').animate( {
 				top: '0',
@@ -182,8 +158,9 @@ $('document').ready(function() {
 	
 	function preSubmit(e) {
 		if ($('#input').val().length != 0) {
-			if (validColor($('#input').val(), cFormats[cIndex])) {
-				submit();
+			if (validColor($('#input').val(), formats[cIndex])) {
+				cPick = validColor($('#input').val(), formats[cIndex])[0];
+				submit(cTrue, cPick, cFormat);
 			} else {
 				$('#input')
 					.css('text-decoration', 'line-through')
@@ -222,6 +199,6 @@ $('document').ready(function() {
 	$('#format').on('click', changeFormat);
 	
 	//init
-	$('body').css('background-color', cPrimary.hex);
-	cContrast(cPrimary.num[0], cPrimary.num[1], cPrimary.num[2])
+	$('body').css('background-color', cTrue.hex);
+	cContrast(cTrue.num[0], cTrue.num[1], cTrue.num[2]);
 });
