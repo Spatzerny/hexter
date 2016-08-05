dc3c79aa2996c180ad4180fbd64e13b2ff8f0861
Spatzerny <spatzerny@gmail.com>
2016-08-05 23:10:52 +0200

    comments, tiny changes and trying to understand my own code

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
+/* random number from a range */
 function randomRange(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function randomColor() {
-	var r = randomRange(0, 255);
-	var g = randomRange(0, 255);
-	var b = randomRange(0, 255);
+/* returns a random object with hex, rgb and hsl color formats (String) as well as array of Array of rgb(Number) */
+/* if provided with r g b arguments - uses those instead of randomising */
+function getColorObject(r,g,b) {
+	var r = r || randomRange(0, 255);
+	var g = g || randomRange(0, 255);
+	var b = b || randomRange(0, 255);
 	
 	return {
 		num: [r,g,b],
@@ -18,6 +21,7 @@ function randomColor() {
 	}
 }
 
+/* just leftpad :P */
 function leftpad (str, len, ch) {
   str = String(str);
   var i = -1;
@@ -29,6 +33,7 @@ function leftpad (str, len, ch) {
   return str;
 }
 
+/* validates and parses a string for an array of values */
 function validateColor(s,f) {
 	s = s.toString();
 	if (f == 'hex') {
@@ -48,14 +53,14 @@ function validateColor(s,f) {
 
 
 // https://github.com/mjackson/mjijackson.github.com/blob/master/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript.txt
-function rgbToHsl(r, g, b){
+function rgbToHsl(r, g, b) {
 		r /= 255, g /= 255, b /= 255;
 		var max = Math.max(r, g, b), min = Math.min(r, g, b);
 		var h, s, l = (max + min) / 2;
 
 		if(max == min){
 				h = s = 0; // achromatic
-		}else{
+		} else {
 				var d = max - min;
 				s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
 				switch(max){
@@ -69,6 +74,30 @@ function rgbToHsl(r, g, b){
 		return [Math.round(h*360), Math.round(s*100), Math.round(l*100)];
 }
 
+function hslToRgb(h, s, l){
+        var r, g, b;
+
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+
+        return [r * 255, g * 255, b * 255];
+    }
 
 $('document').ready(function() {
 
@@ -76,9 +105,10 @@ $('document').ready(function() {
 	var query = true;
 	var cIndex = 0;
 	var cFormat = formats[cIndex];
-	var cTrue = randomColor();
+	var cTrue = getColorObject();
 	var cPick;
 
+	/* calculates wheter the font should be light or dark to be visible */
 	function cContrast(r, g, b) {
 		var brightness;
 		brightness = r * 299 + g * 587 + b * 114;
@@ -92,14 +122,21 @@ $('document').ready(function() {
 		}
 	}
 
+	/* args: color_true, color_picked, color_format */
 	function submit(c1, c2, f) {
+		console.log(c1,c2,f);
 		
 		$('#submit').animate({
+			/* animate the fadeout of the submit button */
 			top: '60px',
 			opacity: 0
 		}, 250, function() {
 			if (query) {
-				
+				/* if 'submit' */
+
+/* NEED TO MAKE A CONVERSION of c2 to c1 format HERE */
+/* and only then compare */
+
 				for (var i=1; i < c1.length; i++) {
 					if (f = 'hex') {
 						c2[i] = c2[i].toLowerCase();
@@ -116,15 +153,27 @@ $('document').ready(function() {
 				
 				query = false;
 				$('#submit').text('next');
-				$('#temp').text(cTrue[f].toString()).fadeIn('fast');
+
+
+				$('#c_true').text(cTrue[f].toString())
+				$('#c_guess').text(cTrue[f].toString())
+				$('#c_guess').text(cTrue[f].toString())
+				$('#c_result').fadeIn('fast');
+
+
 			} else {
-				cTrue = randomColor();
+				/* if 'next' */
+				cTrue = getColorObject();
 				$('body').css('background-color', cTrue.hex);
 				cContrast(cTrue.num[0], cTrue.num[1], cTrue.num[2]);
 				$('#input').val('');
 				query = true;
 				$('#submit').text('done');
-				$('#temp').fadeOut();
+
+
+				$('#c_result').fadeOut();
+
+
 			}
 			$('#submit').css('top', '-60px');
 			$('#submit').animate( {
@@ -159,9 +208,11 @@ $('document').ready(function() {
 	function preSubmit(e) {
 		if ($('#input').val().length != 0) {
 			if (validateColor($('#input').val(), formats[cIndex])) {
+				/* input correct */
 				cPick = validateColor($('#input').val(), formats[cIndex])[0];
 				submit(cTrue, cPick, cFormat);
 			} else {
+				/* input incorrect */
 				$('#input')
 					.css('text-decoration', 'line-through')
 					.animate({
@@ -187,7 +238,7 @@ $('document').ready(function() {
 		$(this).css('width', (s+1)*26+6);
 	});
 	
-	//on enter key
+	//keyboard events
 	$('body').keydown(function(e) {
 		if (e.which == 13) {
 			preSubmit()
