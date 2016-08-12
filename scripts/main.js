4c96397a8c4f0b932f4848a045652382061ae525
Spatzerny <spatzerny@gmail.com>
2016-08-12 12:08:15 +0200

    _

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,30 @@
 function init() {
-			var color = tinycolor.random()
-			$('body').css('background-color', color.toHexString())
+	var color = tinycolor.random()
+	$('body').css('background-color', color.toHexString())
+	return color;
 }
 
 $(document).ready(function() {
-	init();
+
+	var c_actual = init(),
+			c_guess;
 
 	$('#color_input').on('keypress', function(e) {
 
-		if(e.which == 13) {
+		if (e.which == 13) {
+
 			var input = $(this).val();
+			c_guess = tinycolor(input);
 
-			var c_actual = tinycolor.random()
-			var c_guess = tinycolor(input);
+			$('.color_text').empty().append($('<ul>'
+				+'<li>R:'+ (c_guess.toRgb().r - c_actual.toRgb().r) +'</li>'
+				+'<li>G:'+ (c_guess.toRgb().g - c_actual.toRgb().g) +'</li>'
+				+'<li>B:'+ (c_guess.toRgb().b - c_actual.toRgb().b) +'</li>'
+				+'</ul><p>What\'s the color of the background?</p>'))
 
+			c_actual = tinycolor.random()
 			$(this).val('');
-
-			$('body').css('background-color', c_guess.toHexString())
+			$('body').css('background-color', c_actual.toHexString())
 
 		};
 
